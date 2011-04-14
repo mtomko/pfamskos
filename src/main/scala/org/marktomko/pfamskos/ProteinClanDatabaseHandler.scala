24708380c413e212279321881f0eee507f493952
Mark Tomko <mjt0229@gmail.com>
2011-04-13 23:09:02 -0400

    Fixed record type check logic.

--- src/main/scala/org/marktomko/pfamskos/ProteinClanDatabaseHandler.scala
+++ src/main/scala/org/marktomko/pfamskos/ProteinClanDatabaseHandler.scala
@@ -2,10 +2,10 @@ package org.marktomko.pfamskos
 
 class ProteinClanDatabaseHandler(val clanMembershipDB: ClanMembershipDatabase) extends RecordHandler {
   override def apply(record: StockholmRecord): Unit = {
-    // if the record is a protein clan, store its member proteins in the database
-    if (record.memberProteins.size > 0) {
-      for(protein <- record.memberProteins) {
-        clanMembershipDB.addProtein(protein, getAccession(record))
+    // if the record is a protein clan, store its member families in the database
+    if (getType(record) == "Clan") {
+      for(family <- record.memberFamilies) {
+        clanMembershipDB.addProtein(family, getAccession(record))
       }
     }
   }
